09d85f13fa86bab01611cf58dd07699a8fa6656a
Muhammad Rafi <muhraff@gmail.com>
2020-07-20 08:49:10 +1000

    added smoke test for server-side rendering

--- src/test.js
+++ src/test.js
@@ -1,8 +1,19 @@
 /* eslint-env jest */
+import React from 'react'
+import { renderToString } from 'react-dom/server'
 import Slider from '.'
 
 describe('Slider', () => {
   it('is truthy', () => {
     expect(Slider).toBeTruthy()
+  })
+})
+
+describe('Server-side rendering', () => {
+  it('renders on a server without crashing', () => {
+    const slides = [<img src="..." alt="First slide" />, <img src="..." alt="Second slide" />]
+    const renderOnServer = () => renderToString(<Slider initialSlide={1} slides={slides} />)
+
+    expect(renderOnServer).not.toThrow()
   })
 })
